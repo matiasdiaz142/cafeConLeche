0b0a59ac6c2ef92ad62f2b6062a1d11d4401bf5a
matiasdiaz142 <matiasdiaz142@gmail.com>
2018-04-22 15:37:25 -0300

    subiendo cafe

---
+++ cafeConLeche.hs
@@ -0,0 +1,16 @@
+
+--primos = primos' [2..10]
+
+--primos' (x:xs) = id
+primos2 listaInf = filter (  ((==2).length.(filter (==0))) (map (mod num) [1..num]) ) listaInf
+
+esPrimo num = length (filter (==0) (map (mod num) [1..num])) == 2
+
+
+esPrimo2 (x:xs) | length (filter (==0) (map (mod x) [1..x])) == 2 = x:(esPrimo2 xs)
+				| otherwise = (esPrimo2 xs)
+
+
+esPrimo3 num = ((==2).length.(filter (==0))) (map (mod num) [1..num])
+
+
